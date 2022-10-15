b701cb11ca6bbe2b1e6f9e71ae108877fc6b6b57
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-15 15:17:52 +0600

    untill now password validation completed

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+
+    const [error, setError] = useState("")
+
+    const handleRegistration = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+        //password validation
+        if (!/(?=.*[0-9])(?=.*[!@#$%^&*])(^.{6,}$)/.test(password)) {
+            setError("Password must be at least one special character, one number and 6 character length")
+        }
+    }
+
     return (
-        <div>
+        <div className='w-50 mx-auto'>
             <h1 className='text-primary'>Please Make Your Account!</h1>
-            <form>
+            <form onSubmit={handleRegistration}>
                 <div className="mb-3">
                     <label htmlFor="formGroupExampleInput" className="form-label">Your Email:</label>
                     <input type="email" name="email" className="form-control" id="formGroupExampleInput" placeholder="Email...." required />
@@ -13,6 +29,7 @@ const Register = () => {
                     <label htmlFor="formGroupExampleInput2" className="form-label">Email Password:</label>
                     <input type="password" name="password" className="form-control" id="formGroupExampleInput2" placeholder="Password...." required />
                 </div>
+                <input className="btn btn-outline-warning" type="submit" value="Submit"></input>
             </form>
         </div>
     );
