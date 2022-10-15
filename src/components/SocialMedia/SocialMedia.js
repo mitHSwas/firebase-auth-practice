737d143a66ee7b39439690a27d68abced71a0bcb
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-15 13:50:24 +0600

    google sign in and out applied successfully

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import app from '../../firebase/firebase.init';
+
+const auth = getAuth(app)
 
 const SocialMedia = () => {
+    const [user, setUser] = useState({})
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+            })
+            .catch(error => {
+                console.error("error", error)
+            })
+    }
+
+    const handleSingOut = () => {
+        signOut(auth)
+            .then(result => {
+                setUser({})
+                alert("Sign out successfully")
+            })
+            .catch(error => console.error("Error", error))
+    }
+
     return (
         <div>
-            <h1>Here will be social media login</h1>
+            {user.uid ? <div>
+                <button onClick={handleSingOut} type="button" className="btn btn-danger m-2">Sign Out</button>
+            </div>
+                :
+                <div>
+                    <button onClick={handleGoogleSignIn} type="button" className="btn btn-primary m-2">Google Sign In</button>
+                </div>}
+
+            {user.uid && <div>
+                <h1>Name: {user.displayName}</h1>
+                <p>Your Email: {user.email}</p>
+                <img src={user.photURL} alt="" />
+            </div>}
         </div>
     );
 };
